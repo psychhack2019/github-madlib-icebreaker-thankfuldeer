5357dbc92c17ac62fc2a6c4a8e353475e79bbf55
Annabel Fan <19580213+ThreeTreeTrolls@users.noreply.github.com>
2019-05-03 15:17:48 -0400

    updated madlib files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Icebreaker exercise for PyschHack 2019
+Icebreaker exercise for PyschHacks 2019
 
 INSTRUCTIONS
 Make sure everyone on the team has accepted the GitHub Classroom invite,
@@ -10,12 +10,11 @@ and has cloned the repo locally.
 3) Git add -A
 4) Git commit -m"Type in your name"
 5) Git push
-6) Stand up and cheer for the rest of your team!
-7) Next person repeats steps 2 - 6
-8) Last person runs the code to generate the madlib text file. (this person needs to have python installed)
+6) Next person repeats steps 2 - 6
+8) Last person runs the code to generate the madlib text file. 
+(this person needs to have python installed)
 
-Check that the file is created successfully,
-When the team is done have everyone sit back down.
+Check that the file is created successfully!
 
 credits: adapted from PhD Comics
 
--- madlib.py
+++ madlib.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
 """
-@author: annabel wing-yan fan 2019
+Icebreaker exercise for PyschHacks 2019
 
 INSTRUCTIONS
+Make sure everyone on the team has accepted the GitHub Classroom invite,
+and has cloned the repo locally.
 
 1) Git pull (unless you're starting)
 2) Change your word selections, this can be done in any text editor
 3) Git add -A
 4) Git commit -m"Type in your name"
 5) Git push
-6) Stand up and cheer for the rest of your team!
-7) Next person repeats steps 2 - 6
-8) Last person runs the code to generate the madlib text file. (this person needs to have python installed)
+6) Next person repeats steps 2 - 6
+8) Last person runs the code to generate the madlib text file.
+(this person needs to have python installed)
 
-Check that the file is created successfully,
-When the team is done have everyone sit back down.
+Check that the file is created successfully!
 
 credits: adapted from PhD Comics
 """
