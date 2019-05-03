3ddb545d49a694881521e3afca00c015d718fe04
Annabel Fan <19580213+ThreeTreeTrolls@users.noreply.github.com>
2019-05-02 23:02:02 -0400

    final files

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 Icebreaker exercise for PyschHack 2019
 
-Follow the instructions in the python madlib.py script. Each person on the team is responsible for adding a few words to the story. Once you're done stand up and cheer on the rest of your team!
+INSTRUCTIONS
+Make sure everyone on the team has accepted the GitHub Classroom invite,
+and has cloned the repo locally.
 
-Last person to complete the story, should run the madlib.py script, followed by the story.py script to generate a text file with your madlib. Once the file has been generated successfully, have your entire team sit down
 
-Help each other out and have fun!
+1) Git pull (unless you're starting)
+2) Change your word selections, this can be done in any text editor
+3) Git add -A
+4) Git commit -m"Type in your name"
+5) Git push
+6) Stand up and cheer for the rest of your team!
+7) Next person repeats steps 2 - 6
+8) Last person runs the code to generate the madlib text file. (this person needs to have python installed)
+
+Check that the file is created successfully,
+When the team is done have everyone sit back down.
+
+credits: adapted from PhD Comics
 
 story adapted from PhD Comics: https://tapas.io/episode/37261
--- madlib.py
+++ madlib.py
@@ -5,37 +5,78 @@
 INSTRUCTIONS
 
 1) Git pull (unless you're starting)
-2) Change your word selections
+2) Change your word selections, this can be done in any text editor
 3) Git add -A
 4) Git commit -m"Type in your name"
 5) Git push
+6) Stand up and cheer for the rest of your team!
+7) Next person repeats steps 2 - 6
+8) Last person runs the code to generate the madlib text file. (this person needs to have python installed)
 
-Next person!
+Check that the file is created successfully,
+When the team is done have everyone sit back down.
 
 credits: adapted from PhD Comics
 """
 
 #person 1
-teamName = "teamA"
-synonymForNew = "new"
-sciencyVerb = "sciencing"
-buzzword = "buzz"
+teamName = ""
+synonymForNew = ""
+sciencyVerb = ""
+buzzword = ""
 
 #person 2
-number = "1"
-buzzword2 = "buzz buzz"
-sexyAdjective = "reliable"
-somethingYouDidntInvent = "PCR"
+number = ""
+buzzword2 = ""
+sexyAdjective = ""
+somethingYouDidntInvent = ""
 
 
 #person 3
-number2 = "2"
-buzzword3 = "buzzy"
-scientistThatScoopsYourLabConstantly = "Newton"
-dependentVariable = "size"
+number2 = ""
+buzzword3 = ""
+scientistThatScoopsYourLabConstantly = ""
+dependentVariable = ""
 
 #person 4
-units = "cm"
-buzzword4 = "buzzing"
-supremeSocialogicalConcern = "world hunger"
-nounFewPeopleHaveHeardOf = "triglyceride lipase"
+units = ""
+buzzword4 = ""
+supremeSocialogicalConcern = ""
+nounFewPeopleHaveHeardOf = ""
+
+
+
+
+
+
+
+""" STOP HERE! -------------------------------------------------------------"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+story = ("This paper presents a " + synonymForNew + " method for " +
+         sciencyVerb + "\nthe " + nounFewPeopleHaveHeardOf + ". Using " +
+         somethingYouDidntInvent + ", the \n" + dependentVariable + " was measured to be " +
+         number + " +/- " + number2 + "\n" + units + ". " +
+         "Results show " + sexyAdjective + " agreement with \ntheoretical predictions and significant improvement over \nprevious efforts by " +
+         scientistThatScoopsYourLabConstantly + " et al. The work presented \nhere has profound implications for future studies of \n" +
+         buzzword + " and may one day help solve the problem of \n" + supremeSocialogicalConcern + ". \n\n\n"
+
+         "Keywords: " + buzzword2 + ", " +  buzzword3 + ", " +  buzzword4
+        )
+
+
+madlib = open(teamName + "_madlib.txt", "w")
+madlib.write(story)
+madlib.close()
